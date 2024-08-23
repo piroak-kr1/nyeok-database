847ca280481feec5f1115156842aeec43082d1f6
kimkun07 <kimkun07@naver.com>
2024-08-23 19:24:53 +0900

    db에 대한 초기화, db expose

--- database-core/database_core/db.py
+++ database-core/database_core/db.py
@@ -1,23 +1,44 @@
 # Setup database connection and expose
 
 from typing import Any, Generator
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-
-# NOTE: postgres trust local connections
-# TODO: make as environment variable (also in alembic.ini)
-engine = create_engine("postgresql://superuser:wrong_password@localhost:5432/database")
-
-# NOTE: Do not create table - Alembic will manage schema
-
-SessionLocal = sessionmaker(bind=engine)
+from .with_enforcer import WithEnforcer
 
 
-def get_db() -> Generator[Session, Any, None]:
-    db = SessionLocal()
+_engine: Engine | None = None
+_SessionLocal: sessionmaker[Session] | None = None
+
+
+def setup(username: str, password: str, hostname: str, port: int, databasename: str):
+    """User should setup database first"""
+    global _engine, _SessionLocal
+
+    # NOTE: postgres trust local connections, so may not need to provide password
+    _engine = create_engine(
+        f"postgresql://{username}:{password}@{hostname}:{port}/{databasename}"
+    )
+    _SessionLocal = sessionmaker(bind=_engine)
+
+
+def get_session_yield() -> Generator[Session, Any, None]:
+    """Used by FastAPI dependency"""
+    if _SessionLocal is None:
+        raise Exception("Databse has not been initialized")
+
+    session: Session = _SessionLocal()
     try:
-        yield db
+        yield session
     finally:
-        db.close()
+        session.close()
+
+
+def get_session_with() -> Session:
+    """Used by non-FastAPI user"""
+    if _SessionLocal is None:
+        raise Exception("Databse has not been initialized")
+
+    session: Session = _SessionLocal()
+    return WithEnforcer(session)  # type: ignore[return-type]
